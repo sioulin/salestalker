7f8083074898e0b0f640d71649e8abbfc6978086
Siou Lin <silin@expedia.com>
2018-02-25 20:42:24 -0600

    Spring boot initializer 2.0.0 kotlin + webflux

---
+++ src/main/kotlin/com/slin/salestalker/SalestalkerApplication.kt
@@ -0,0 +1,11 @@
+package com.slin.salestalker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SalestalkerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SalestalkerApplication>(*args)
+}
